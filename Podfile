5619b3748988d6639c62454652c3eef20cbd1ddc
iqbalansyori <iqbal@icehousecorp.com>
2017-04-05 18:08:00 +0700

    Add carthage support - 1.1.3

---
+++ Podfile
@@ -0,0 +1,5 @@
+use_frameworks!
+
+target 'APNumberPad' do
+  pod 'APNumberPad', '1.1.3'
+end
---
+++ Pods/Target Support Files/APNumberPad/APNumberPad-umbrella.h
@@ -0,0 +1,12 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "APNumberButton.h"
+#import "APNumberPad.h"
+#import "APNumberPadDefaultStyle.h"
+#import "APNumberPadStyle.h"
+
+FOUNDATION_EXPORT double APNumberPadVersionNumber;
+FOUNDATION_EXPORT const unsigned char APNumberPadVersionString[];
+
